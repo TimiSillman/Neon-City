dd9d4e74cb23c638aa61993bb3f8a995d1f5b4cd
Olli Juhola <juhola.olli@gmail.com>
2018-01-26 12:39:33 +0200

    Added movement mid air
    Wall jumping still buggy

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 6.0F;
-    public float jumpForce = 8.0F;
+    public float speed = 20;
+    public float jumpForce = 15;
     public float wallJumpSpeed = -0.7f;
 
 
     //Private Variables
     Vector3 moveDirection = Vector3.zero;
     CharacterController controller;
-    float gravity = 30.0F;
+    float gravity = 30f;
 
     private void Start()
     {
@@ -30,12 +30,27 @@ public class PlayerController : MonoBehaviour
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
             moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
-
+            moveDirection.x *= speed;
             if (Input.GetButton("Jump"))
+            {
                 moveDirection.y = jumpForce;
+            }
+            
+        } else if (!controller.isGrounded)
+        {
 
+            //Tässä siis yritän tehdä että voi liikkua ilmassa, jos tossa ei ole tota GetButton(Horizontal) niin sitten se ei launchaa seinästä niinkuin pitäisi
+
+            //Ongelma: Jos painaa A/D ja sitte Jump niin se kiipee pelkästää ylöspäin, mut jos ei paina noita niin launchaa hyvin, pitää siis saada joku esto
+            //          Että se ei pysty kiipee ylöspäin, että aina launchaa
+            if (Input.GetButton("Horizontal"))
+            {
+                moveDirection.x = Input.GetAxis("Horizontal");
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection.x *= speed;
+            }
         }
+
 
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
@@ -44,17 +59,16 @@ public class PlayerController : MonoBehaviour
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if (!controller.isGrounded && hit.normal.y < 0.1f)
+        if (!controller.isGrounded && hit.normal.y < 0.01f)
         {
-
             if (Input.GetButtonDown("Jump"))
             {
                 Debug.Log("HIT");
                 moveDirection.y = jumpForce;
                 moveDirection.x = moveDirection.x * wallJumpSpeed;
             }
+            
         }
     }
-
 
 }
