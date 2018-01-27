fccc641d3670800d2ed51feb7bb5146011edb60e
Olli Juhola <juhola.olli@gmail.com>
2018-01-27 13:47:16 +0200

    Added a new Controller Script
    Test if its working better than previous

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,13 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 20;
     public float jumpForce = 15;
-    public float wallJumpSpeed = -0.7f;
-
+    public float wallJumpSpeed = -1f;
+    public float speedSmooth;
 
     //Private Variables
-    Vector3 moveDirection = Vector3.zero;
+    public Vector3 moveDirection = Vector3.zero;
     CharacterController controller;
     float gravity = 30f;
+    Vector3 currentSpeed = Vector3.zero;
 
     private void Start()
     {
@@ -26,6 +27,7 @@ public class PlayerController : MonoBehaviour
 
     void Movement()
     {
+
         if (controller.isGrounded)
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
@@ -38,11 +40,6 @@ public class PlayerController : MonoBehaviour
             
         } else if (!controller.isGrounded)
         {
-
-            //Tässä siis yritän tehdä että voi liikkua ilmassa, jos tossa ei ole tota GetButton(Horizontal) niin sitten se ei launchaa seinästä niinkuin pitäisi
-
-            //Ongelma: Jos painaa A/D ja sitte Jump niin se kiipee pelkästää ylöspäin, mut jos ei paina noita niin launchaa hyvin, pitää siis saada joku esto
-            //          Että se ei pysty kiipee ylöspäin, että aina launchaa
             if (Input.GetButton("Horizontal"))
             {
                 moveDirection.x = Input.GetAxis("Horizontal");
@@ -54,18 +51,21 @@ public class PlayerController : MonoBehaviour
 
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+        currentSpeed = moveDirection;
     }
 
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if (!controller.isGrounded && hit.normal.y < 0.01f)
+ 
+
+        if (!controller.isGrounded && hit.normal.y < 0.1f)
         {
             if (Input.GetButtonDown("Jump"))
             {
                 Debug.Log("HIT");
                 moveDirection.y = jumpForce;
-                moveDirection.x = moveDirection.x * wallJumpSpeed;
+                moveDirection.x *= wallJumpSpeed;
             }
             
         }
---
+++ Assets/scripts/PlayerController2.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour {
+
+    public float groundSpeed = 3f;
+    public float airSpeed = 2f;
+    public float gravityStrength = 5f;
+    public float jumpForce = 10f;
+
+    bool canJump = false;
+    float verticalVelocity;
+    Vector3 velocity;
+    Vector3 groundedVelocity;
+    Vector3 normal;
+    bool onWall = false;
+
+    CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+        cc = this.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 playerVector = Vector3.zero;
+        Vector3 input = Vector3.zero;
+
+        //get input
+        input.x = Input.GetAxis("Horizontal");
+        input = Vector3.ClampMagnitude(input, 1f);
+
+        if (cc.isGrounded)
+        {
+            //if grounded, get the basic input the player gives with the basic ground speed
+            playerVector = input;
+            playerVector *= groundSpeed;
+
+        } else
+        {
+            //If in air, the speed is set by airSpeed variable * the Vector 3 input
+            playerVector = groundedVelocity;
+            playerVector += input * airSpeed;
+        }
+
+        //clamp the speed so it does not go over the ground speed level (no BHOP)
+        playerVector = Vector3.ClampMagnitude(playerVector, groundSpeed);
+        playerVector *= Time.deltaTime;
+
+
+
+        verticalVelocity -= (gravityStrength * Time.deltaTime);
+
+        if (Input.GetButton("Jump"))
+        {
+            //If touching wall, ability to double jump into an arc
+            if (onWall)
+            {
+                Vector3 reflection = Vector3.Reflect(velocity, normal);
+                Vector3 projected = Vector3.ProjectOnPlane(reflection, Vector3.up);
+                groundedVelocity = projected.normalized * groundSpeed + normal * airSpeed;
+            }
+            if (canJump)
+            {
+                //basic jump
+                verticalVelocity = jumpForce;
+            }         
+        }
+
+        playerVector.y = verticalVelocity * Time.deltaTime;
+
+        //EI VITTU MITÄÄ HAJUA
+        CollisionFlags flags = cc.Move(playerVector);
+        velocity = playerVector / Time.deltaTime;
+        //use of flags to determine if character can jump or not
+        //if on ground
+        //set canjump to true
+        if ((flags & CollisionFlags.Below) != 0)
+        {
+            groundedVelocity = Vector3.ProjectOnPlane(velocity, Vector3.up);
+            canJump = true;
+            verticalVelocity = -3f;
+            onWall = false;
+        }
+        else if ((flags & CollisionFlags.Sides) != 0) {
+
+            canJump = true;
+            onWall = true;
+        }
+        else
+        {
+            canJump = false;
+            onWall = false;
+        }
+
+
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        //Gets the Vector value for the hitpoint
+        normal = hit.normal;  
+    }
+}
